b6862fc64cf2e439e3351765c22f6111ffedc01c
Tanvir Ahmad <tnvir9@gmail.com>
2023-09-18 10:52:16 +0600

    Display the count

--- index.js
+++ index.js
@@ -38,17 +38,40 @@
 
 // // *** Write a functions that increments ***
 
-let lapsCompleted = 0
+// let lapsCompleted = 0
 
 // Create a function that increments the lapsCompleted variable with one
 // Run it three times
 
-function increaseLap() {
-    lapsCompleted = lapsCompleted + 1
-    console.log(lapsCompleted)
-}
-increaseLap()
-increaseLap()
-increaseLap()
+// function increaseLap() {
+//     lapsCompleted = lapsCompleted + 1
+//     console.log(lapsCompleted)
+// }
+// increaseLap()
+// increaseLap()
+// increaseLap()
 
 // console.log(lapsCompleted)
+
+// // *** Increments on click ***
+
+// intialize the count as 0
+let count = 0
+
+// listen for clicks on the increment button
+function increment(){
+    console.log("The button was clicked.")
+}
+
+// increment the count variable when the button is clicked (log it out)
+function increment(){
+    count = count +1
+    console.log(count)
+}
+increment()
+
+// change the count-el in the HTML to reflect the new count
+function increment(){
+    count = count + 1
+    document.getElementById("count-el").innerHTML = count
+}
