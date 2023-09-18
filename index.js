7e9bbdb1dd01be5effc71a5a0a2e05c1c39ddce4
Tanvir Ahmad <tnvir9@gmail.com>
2023-09-18 10:25:46 +0600

    Write a functions that increments.

--- index.js
+++ index.js
@@ -4,13 +4,13 @@
 
 // // *** Using Functions to Write Less Code ***
 
-function countDown() {
-    console.log(5)
-    console.log(4)
-    console.log(3)
-    console.log(2)
-    console.log(1)
-}
+// function countDown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
 
 // Setting up the the race 🏎 🏎 🏎
 // countDown()
@@ -24,14 +24,31 @@ function countDown() {
 
 // // *** Write a function that logs the sum ***
 
-let lap1 = 34
-let lap2 = 33
-let lap3 = 36
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
 
 // Create a function that logs out the sum of all the lap times
-function totalLapTime(){
-    let lapTime = lap1 + lap2 + lap3
-    console.log(lapTime)
-}
+// function totalLapTime(){
+//     let lapTime = lap1 + lap2 + lap3
+//     console.log(lapTime)
+// }
 
-totalLapTime()
+// totalLapTime()
+
+// // *** Write a functions that increments ***
+
+let lapsCompleted = 0
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+function increaseLap() {
+    lapsCompleted = lapsCompleted + 1
+    console.log(lapsCompleted)
+}
+increaseLap()
+increaseLap()
+increaseLap()
+
+// console.log(lapsCompleted)
